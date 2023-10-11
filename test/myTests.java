97f6e2fe135d56045bb7f47e5342846d0e47d95c
ryannav <mrnavar2@asu.edu>
2023-10-11 01:47:41 -0700

    All features and tests work, project is completed

--- test/myTests.java
+++ test/myTests.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 
 public class myTests {
 
-
+//-------------------- Start of feature 1 tests -----------------------------------------------------
     @Test
     public void testfeatur1pt1(){        //this test will cover feature 1 in the project
         main s = new main();
@@ -25,6 +25,21 @@ public class myTests {
                 "\"A\" -> \"D\"\n" +
                 "}", s.tostring());
     }
+    @Test
+    public void testfeatur1pt1test2(){        //this test will cover feature 1 in the project
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        assertEquals("Your vertexes: [A, B, C, D, F]\n" +
+                "Number of vertexes in your graph: 5\n" +
+                "Number of edges in your graph: 5\n" +
+                "digraph \"A\" {\n" +
+                "\"A\" -> \"C\"\n" +
+                "\"A\" -> \"B\"\n" +
+                "\"C\" -> \"D\"\n" +
+                "\"B\" -> \"C\"\n" +
+                "\"B\" -> \"F\"\n" +
+                "}", s.tostring());
+    }
 
     @Test
     public void testfeatur1pt2(){
@@ -33,12 +48,28 @@ public class myTests {
         try{
             String exStr = Files.readString(Paths.get("expectedOut/f1pt2expected.txt"));
             assertEquals(exStr.replaceAll("\n", "").replaceAll("\r", ""), s.outputGraph("outputs/testf1out.txt").replaceAll("\n", "").replaceAll("\r", ""));
-            //added replaceall to text because it would fail the test due to "line seperators" between the two despite being the same so I touched the strngs up for the test ( they are the same either way)
+            //added replace all to text because it would fail the test due to "line seperators" between the two despite being the same so I touched the strngs up for the test ( they are the same either way)
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
+    @Test
+    public void testfeatur1pt2test2(){
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        try{
+            String exStr = Files.readString(Paths.get("expectedOut/f1p2t2expected.txt"));
+            assertEquals(exStr.replaceAll("\n", "").replaceAll("\r", ""), s.outputGraph("outputs/testf1pt2out.txt").replaceAll("\n", "").replaceAll("\r", ""));
+            //added replace all to text because it would fail the test due to "line seperators" between the two despite being the same so I touched the strngs up for the test ( they are the same either way)
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //-------------------- Start of feature 2 tests -----------------------------------------------------
     @Test
     public void testfeature2pt1(){                        //tests the single tostring for graphs
         main s = new main();
@@ -52,6 +83,24 @@ public class myTests {
                 "\"A\" -> \"B\"\n" +
                 "\"A\" -> \"C\"\n" +
                 "\"A\" -> \"D\"\n" +
+                "}");
+    }
+
+    @Test
+    public void testfeature2pt1test2(){                        //tests the single tostring for graphs
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        s.addNode("E");
+        assertEquals(s.tostring(), "Your vertexes: [A, B, C, D, E, F]\n" +
+                "Number of vertexes in your graph: 6\n" +
+                "Number of edges in your graph: 5\n" +
+                "digraph \"A\" {\n" +
+                "\"E\"\n" +
+                "\"A\" -> \"C\"\n" +
+                "\"A\" -> \"B\"\n" +
+                "\"C\" -> \"D\"\n" +
+                "\"B\" -> \"C\"\n" +
+                "\"B\" -> \"F\"\n" +
                 "}");
     }
     @Test
@@ -73,6 +122,29 @@ public class myTests {
     }
 
     @Test
+    public void testfeature2pt2test2(){                        //tests the single tostring for graphs
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        String[] j = {"E","G","H","I"};
+        s.addNodes(j);
+        assertEquals(s.tostring(), "Your vertexes: [A, B, C, D, E, F, G, H, I]\n" +
+                "Number of vertexes in your graph: 9\n" +
+                "Number of edges in your graph: 5\n" +
+                "digraph \"A\" {\n" +
+                "\"E\"\n" +
+                "\"G\"\n" +
+                "\"H\"\n" +
+                "\"I\"\n" +
+                "\"A\" -> \"C\"\n" +
+                "\"A\" -> \"B\"\n" +
+                "\"C\" -> \"D\"\n" +
+                "\"B\" -> \"C\"\n" +
+                "\"B\" -> \"F\"\n" +
+                "}");
+    }
+
+    //-------------------- Start of feature 3 tests -----------------------------------------------------
+    @Test
     public void testfeature3(){
         main s = new main();
         s.parseGraph("/test1.dot");
@@ -87,6 +159,29 @@ public class myTests {
                 "\"B\" -> \"C\"\n" +
                 "}");
     }
+
+    @Test
+    public void testfeature3test2(){
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        s.addNode("I");
+        s.addEdge("A","I");
+        assertEquals(s.tostring(), "Your vertexes: [A, B, C, D, F, I]\n" +
+                "Number of vertexes in your graph: 6\n" +
+                "Number of edges in your graph: 6\n" +
+                "digraph \"A\" {\n" +
+                "\"I\"\n" +
+                "\"A\" -> \"I\"\n" +
+                "\"A\" -> \"C\"\n" +
+                "\"A\" -> \"B\"\n" +
+                "\"C\" -> \"D\"\n" +
+                "\"B\" -> \"C\"\n" +
+                "\"B\" -> \"F\"\n" +
+                "}");
+    }
+
+
+    //-------------------- Start of feature 4 tests -----------------------------------------------------
     @Test
     public void testfeature4part1(){
         main s = new main();
@@ -100,10 +195,28 @@ public class myTests {
     }
 
     @Test
+    public void testfeature4part1test2(){
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        s.addEdge("A","F");
+        s.outputDOTGraph("outputs/f4test2Output.dot");
+        String str = s.tostring();
+        main p = new main();
+        p.parseGraph("f4expectedDot2.dot");
+        assertEquals(p.tostring(),str);
+    }
+    @Test
     public void testfeature4part2(){
         main s = new main();
         s.parseGraph("/test1.dot");
         s.addEdge("B","C");
         assertEquals(true, s.outputGraphics("outputs/test2Output.png", "png"));
+    }
+
+    @Test
+    public void testFeature4Part2Test2(){
+        main s = new main();
+        s.parseGraph("/test2.dot");
+        assertEquals(true, s.outputGraphics("outputs/f4t2output.png", "png"));
     }
 }
